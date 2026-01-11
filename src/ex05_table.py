2e1dd6c192a36894ad0e97f64d5b21402d968e96
irati <ilopezvald1@educacion.navarra.es>
2026-01-11 22:25:56 +0100

    Todos los ejercicios

--- src/ex05_table.py
+++ src/ex05_table.py
@@ -8,4 +8,10 @@ def multiplication_table(n: int) -> list[int]:
     Devuelve una lista con 10 elementos:
     [n*1, n*2, ..., n*10]
     """
+    tabla = []
+    i = 1
+    while i <= 10:
+        tabla.append(n * i)
+        i += 1
+    return tabla 
     raise NotImplementedError("Implementa multiplication_table(n)")
